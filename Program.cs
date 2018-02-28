a89c63c13fcbe6e5be453b0d11c59446f4d4c5c8
Bence Makkos <bence.makkos@gmail.com>
2018-02-28 19:43:31 +0100

    initial check-in

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace DreamHome
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            var repositories = ProcessRepositories().Result;
+            foreach (var repo in repositories)
+            {
+                System.Console.WriteLine($"Repository: {repo.Name}, Watchers: {repo.Watchers}, Last push: {repo.LastPush}");
+            }
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+
+            var streamObject = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(streamObject) as List<Repository>;
+
+            return repositories;
+        }
+    }
+
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name="html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name="homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name="watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name="pushed_at")]
+        private string JsonDate { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
